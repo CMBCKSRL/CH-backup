6b8ce447a279b3405c3cb767af70b503ed9a9fa2
Kirill Mankov <93572191+CMBCKSRL@users.noreply.github.com>
2023-08-25 18:20:58 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-#-----------------------------------------------------------------------------------------------------------------------
-#-----------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------
 # Script that creates full and incremental backups of all tables in a particular database.
-# Full backups are made on tuesday and on friday.
-# Backups are stored in files /data/chstore-backup/<database_name>/<backup_date>_<backup_type>/<table_name>.zip
+# Full backups are made on the first day of every month.
+# Backups are stored in files /data/chstore-backup/<cluster_name>/<shard_number>/<database_name>/<backup_date>_<backup_type>/<table_name>.zip
 # where backup_date is written in YYYYMMDD format and backup_type means full backup or incremental one.
 #
 # The script checks if any backup of a certain table  has already been made. 
 # If false (backup table does not exist) then full backup is made no matter what.
 # If any backup file was found then at least one full backup is made and the script will create an incremental one.
-#
-#-----------------------------------------------------------------------------------------------------------------------
-#-----------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------
 
 
-#-----------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------
 # Global variables
-#-----------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------
 # CH creds.
 user=<user_name>
 pass=<password>
@@ -33,64 +32,67 @@ tables=$($docker clickhouse-client -u "$user" --password "$pass" \
 	   WHERE database = '$database'
 	   FORMAT TabSeparated;")
 
-# Get date format for backup directory name and get the day of the week.
+# Get shard number for backup directory name
+shard_num=$($docker clickhouse-client -u "$user" --password "$pass" \
+	-q "SELECT shard_num
+	    FROM system.clusters
+	    WHERE host_name=hostName();")
+
+# Get cluster name which is written in 'database' macros
+cluster=$($docker clickhouse-client -u "$user" --password "$pass" \
+	-q "SELECT getMacro('database');")
+
+# Get date format for backup directory name and get the day of the month.
 date=$(date +%Y%m%d)
-week_day=$(date +%u)
+month_day=$(date +%d)
 
-
-#-----------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------
 # Functions definition
-#-----------------------------------------------------------------------------------------------------------------------
-do_full_backup() {
+#------------------------------------------------------------------------------------------------------------------------------
+do_full_backup() { 
 	$docker clickhouse-client -u "$user" --password "$pass" \
-		-q "BACKUP TABLE "$1"."$2" TO Disk('backup_disk', '"$1"/"$date"_full/"$2".zip');"
+		-q "BACKUP TABLE "$database"."$1" TO Disk('backup_disk', '"$cluster"/"$shard_num"/"$database"/"$date"_full/"$1".zip');"
 }
 
 do_incremental_backup() {
 	$docker clickhouse-client -u "$user" --password "$pass" \
-		-q "BACKUP TABLE "$1"."$2" TO Disk('backup_disk', '"$1"/"$date"_incr/"$2".zip')
-		    SETTINGS base_backup=${3//\\/};"
+		-q "BACKUP TABLE "$database"."$1" TO Disk('backup_disk', '"$cluster"/"$shard_num"/"$database"/"$date"_incr/"$1".zip')
+			SETTINGS base_backup=Disk('backup_disk', '"$cluster"/"$shard_num"/"$database"/"$2"/"$1".zip');"
 }
 
 # Base backup is needed for incremental one.
 get_base_backup() {
-	$docker clickhouse-client -u "$user" --password "$pass" \
-		-q "SELECT name
-		    FROM system.backups
-		    WHERE status=1
-		    AND name LIKE '%\'"$1"/%/"$2"%'
-		    AND end_time > now() - INTERVAL 10 DAY
-		    ORDER BY start_time DESC
-		    LIMIT 1;"
+	$docker bash -c "ls -rt /data/chstore-backup/"$cluster"/"$shard_num/"$database"/*/"$1".zip" | tail -1" | awk -F"/" '{print $7}' 
 }
 
 # Get the number of backup files for a certain table.
 num_backup_files() {
-	$docker find /data/chstore-backup/"$1" -name "$2.zip" | wc -l 
+	$docker find /data/chstore-backup/"$cluster"/"$shard_num"/"$database" -name "$1.zip" | wc -l 
 }
 
 
-#-----------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------
 # Start script
-#-----------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------
 
 main() {
 
-	if [[ "$week_day" -eq 2 ]] || [[ "$week_day" -eq 5 ]] ; then	# Check day of the week number
+	if [[ "$month_day" -eq 1 ]] ; then	# Check day of the month number
 		
 		for table in $tables
 		do	
-			do_full_backup "$database" "$table"
+			do_full_backup "$table"
 		done
 	else
 		for table in $tables
 		do	
-			if [[ $(num_backup_files "$database" "$table") -ne 0 ]] ; then	# Check if there is any backup file for every table.
-											# If the file exists than create incremental backup.
-				base_backup=$(get_base_backup "$database" "$table")
-				do_incremental_backup "$database" "$table" "$base_backup"
+			if [[ $(num_backup_files "$table") -ne 0 ]] ; then	
+									# Check if there is any backup file for every table.
+									# If the file exists than create incremental backup.
+				base_backup=$(get_base_backup "$table")
+				do_incremental_backup "$table" "$base_backup"
 			else
-				do_full_backup "$database" "$table"
+				do_full_backup "$table"
 			fi	
 		done
 	fi
